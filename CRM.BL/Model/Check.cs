1ee01e1827ad9679937d32f1d2036765837bde6a
DimaCh <chdmitr2@gmail.com>
2021-03-14 17:01:01 +0200

    Add project files.

---
+++ CRM.BL/Model/Check.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.BL.Model
+{
+    public class Check
+    {
+        public int CheckId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int SellerId { get; set; }
+
+        public virtual Seller Seller { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public virtual ICollection<Sell> Sells { get; set; }
+
+        public override string ToString()
+        {
+            return $"No.{CheckId} from {Created.ToString("dd.MM.yy hh:mm:ss")}";
+        }
+    }
+}
